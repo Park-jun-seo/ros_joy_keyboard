42edc07795917c1aac284f78019dd1974978e65c
pjs <pjsyukizang@naver.com>
2023-01-20 16:50:40 +0900

    0120

--- src/ros_joy_keyboard.cpp
+++ src/ros_joy_keyboard.cpp
@@ -9,6 +9,8 @@ ros::Publisher joy_pub;
 sensor_msgs::Joy joy_msg;
 
 int Ascii[] = {96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 68, 65, 67, 66}; //{`,1,2,3,4,5,6,7,8,9,0,←,↑,→,↓}
+int key;
+
 
 std::string msg = R"(
 
@@ -16,7 +18,7 @@ std::string msg = R"(
  buttons:  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
  key borad:[`, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 
- axes   :  [0, 0, 0, 0, 0, 0]
+ axes   :  [←→, ↑↓, 0, 0, 0, 0]
  key borad:[←, ↑, →, ↓]
  
  CTRL-C to quit
@@ -28,7 +30,7 @@ int Getch(void) // #include <termios.h> 참조
     int ch;
     struct termios oldt;
     struct termios newt;
-
+    
     // Store old settings, and copy to new settings
     tcgetattr(STDIN_FILENO, &oldt);
     newt = oldt;
@@ -38,17 +40,23 @@ int Getch(void) // #include <termios.h> 참조
     newt.c_iflag |= IGNBRK;
     newt.c_iflag &= ~(INLCR | ICRNL | IXON | IXOFF);
     newt.c_lflag &= ~(ICANON | ECHO | ECHOK | ECHOE | ECHONL | ISIG | IEXTEN);
-    newt.c_cc[VMIN] = 1;
+    newt.c_cc[VMIN] = 0;
     newt.c_cc[VTIME] = 0;
     tcsetattr(fileno(stdin), TCSANOW, &newt);
 
+    int input = 0;
+    if(read(STDIN_FILENO,&input,1) !=1)
+    {
+        input = 0;
+    }
+    
     // Get the current character
-    ch = getchar();
+    //ch = getchar();
 
     // Reapply old settings
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
 
-    return ch;
+    return input;
 }
 
 void JoyInit() // Joy 버튼 초기화
@@ -137,13 +145,13 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "joy_keyboard");
     ros::NodeHandle n;
     joy_pub = n.advertise<sensor_msgs::Joy>("/joy", 1);
-
-    ros::Rate loop_rate(1000);
-
+    ros::Rate loop_rate(10);
+    
     while (ros::ok())
     {
+
         std::cout << msg;
-        int key = Getch();
+        key = Getch();
         ROS_INFO_STREAM(key);
         if (key == 3) // ctrl+c
         {
@@ -153,10 +161,16 @@ int main(int argc, char **argv)
         {
             continue;
         }
-        
-        JoyKeboard(key);
-        joy_pub.publish(joy_msg);
-
+        if(key!=-1)
+        {
+            JoyKeboard(key);
+            joy_pub.publish(joy_msg);
+        }
+        else if(key ==-1)
+        {
+            JoyInit();
+            joy_pub.publish(joy_msg);
+        }
         ros::spinOnce();
         loop_rate.sleep();
     }
